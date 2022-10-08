7627b740934a2f293b85a0bf8e89d58198a0ffb9
mirko-san <41379069+mirko-san@users.noreply.github.com>
2022-10-09 01:51:47 +0900

    :sparkles: カスタムエレメントの利用に必要そうなビルド設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: './src/main.ts',
+      name: 'MClock',
+      fileName: (format) => `m-clock.${format}.js`,
+    },
+  },
   plugins: [
     svelte({
       compilerOptions: {
