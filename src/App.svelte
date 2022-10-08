cee3b4ebe1684f48bf57a079527197b78241166f
mirko-san <41379069+mirko-san@users.noreply.github.com>
2022-10-08 11:30:13 +0900

    :tada: 想定成果物に向けて initialize

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,11 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import MClock from './lib/MClock.svelte'
 </script>
 
 <main>
   <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    <MClock />
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
---
+++ src/lib/MClock.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { onMount, onDestroy } from "svelte";
+  let date = new Date().toLocaleTimeString();
+  let interval;
+
+  onMount(() => {
+    interval = setInterval(() => {
+      date = new Date().toLocaleTimeString();
+    }, 1000);
+  });
+
+  onDestroy(() => {
+    clearInterval(interval);
+  });
+</script>
+
+<div>
+  <span>{date || ""}</span>
+</div>
+
+<style>
+  div {
+    color: black;
+  }
+</style>
